06454229270946ccd313491398b0d67519d0540e
aishwaryak-01 <133705983+aishwaryak-01@users.noreply.github.com>
2023-10-04 09:16:20 +0530

    app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const errorControllers = require('./controllers/error');
 
 const app = express();
 
@@ -12,19 +13,15 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const contactRoutes = require('./routes/contactus');
 const successRoutes = require('./routes/success');
-const errorRoutes = require('./routes/404');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(contactRoutes);
 app.use(successRoutes);
-app.use(errorRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorControllers.get404);
 
 app.listen(3000);
